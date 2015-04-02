990cd1b05c99166eac1de7bba67af4808e416c3b
Larry Zhang <lzhang124@gmail.com>
2015-04-01 20:18:18 -0400

    init

---
+++ Timeline/TimelineView.swift
@@ -0,0 +1,16 @@
+//
+//  TimelineView.swift
+//  Timeline
+//
+//  Created by Larry Zhang on 4/1/15.
+//  Copyright (c) 2015 Larry Zhang. All rights reserved.
+//
+
+import UIKit
+
+public struct TimeFrame{
+    let time: String
+    let image: UIImage?
+}
+
+
